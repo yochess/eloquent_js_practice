b1eff3af61878369269b007768a3ebeafc574378
Daham Drake Wang <drakewang@gmail.com>
2015-11-19 08:18:08 -0800

    added flatten.js

---
+++ chapter_5/flatten.js
@@ -0,0 +1,16 @@
+/*
+Chapter 5 Exercises - Flattening
+
+Use the reduce method in combination with the concat method to “flatten” an 
+array of arrays into a single array that has all the elements of the input 
+arrays.
+*/
+
+function flatten(arys) {
+  return arys.reduce(function(memo, ary) {
+    return memo.concat(ary);
+  });
+};
+
+var arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(flatten(arrays)); // => [1, 2, 3, 4, 5, 6]
