d61baf379ba7aafd9e406f02530b97fe82573a88
Daham Drake Wang <drakewang@gmail.com>
2015-11-19 02:56:50 -0800

    unwrap prompt

--- chapter_3/findSolution.js
+++ chapter_3/findSolution.js
@@ -1,7 +1,13 @@
 /*
 Chapter 3: Functions
 
-Consider this puzzle: by starting from the number 1 and repeatedly either adding 5 or multiplying by 3, an infinite amount of new numbers can be produced. How would you write a function that, given a number, tries to find a sequence of such additions and multiplications that produce that number? For example, the number 13 could be reached by first multiplying by 3 and then adding 5 twice, whereas the number 15 cannot be reached at all.
+Consider this puzzle: by starting from the number 1 and repeatedly either 
+adding 5 or multiplying by 3, an infinite amount of new numbers can be 
+produced. How would you write a function that, given a number, tries to 
+find a sequence of such additions and multiplications that produce that 
+number? For example, the number 13 could be reached by first multiplying 
+by 3 and then adding 5 twice, whereas the number 15 cannot be reached at 
+all.
 
 */
 
@@ -84,6 +90,8 @@ console.log( findSolution( 231 ) ); // => ((((((((((((((((((((((((((((((((((((((
 // search for values that make sense FIRST 
 // IT MAKES WAY MORE SENSE TO MULTIPLY FIRST NOW!!
 
+
+/* FINAL ANSWER */
 function findSolution_2( target ) {
   var total  = 1;
   var output = "1";
@@ -99,6 +107,6 @@ function findSolution_2( target ) {
   return find_2( total, output );
 }
 
-console.log( findSolution( 231 ) ); => ((((((((((((((((((((((((((((((((((1 * 3) * 3) * 3) * 3) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5)
+console.log( findSolution( 231 ) ); //=> ((((((((((((((((((((((((((((((((((1 * 3) * 3) * 3) * 3) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5) + 5)
 
 // ok only SLIGHTLY better!
